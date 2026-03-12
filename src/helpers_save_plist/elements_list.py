7ba830443c3fbf9994d5648193cfc0dca3bfa57e
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-12 20:33:35 +0100

    restore_elements_to_og

--- src/helpers_save_plist/elements_list.py
+++ src/helpers_save_plist/elements_list.py
@@ -29,19 +29,25 @@ class Elements_List():
         if len(urls_list) != len(names_list):
             raise ValueError("Lists not of the same length")
 
-        self.og_urls_list    = urls_list
-        self.og_names_list   = names_list
-        self.og_len          = len(urls_list)
-        self.new_urls_list   = urls_list
-        self.new_names_list  = names_list
-        self.new_index_in_og = [i for i in range(len(urls_list))]
-        self.new_len         = len(urls_list)
-        self.numbering       = numbering
+        self.og_urls_list   = urls_list
+        self.og_names_list  = names_list
+        self.og_len         = len(urls_list)
+
+        self.new_urls_list  = urls_list
+        self.new_names_list = names_list
+        self.new_len        = len(urls_list)
+        self.numbering      = numbering
         self.numbering_has_zeros = numbering_has_zeros
         self.del_duplicates  = del_duplicates
 
+        self.reset_new_index_in_og()
         self.calc_numbering_list()
         self.delete_duplicates_accordingly()
+
+
+    def reset_new_index_in_og(self):
+        self.new_index_in_og = [
+            i for i in range(len(self.new_urls_list))]
 
 
     def calc_numbering_list(self) -> None:
@@ -69,6 +75,18 @@ class Elements_List():
         if not self.del_duplicates:
             return
         self._delete_duplicates()
+
+
+    def restore_elements_to_og(self) -> None:
+        self.new_urls_list = self.og_urls_list.copy()
+        temp_names_list = self.og_names_list.copy()
+        for index in range(self.new_index_in_og):
+            changed_name = self.new_names_list[index]
+            index_to_change = self.new_index_in_og[index]
+            temp_names_list[index_to_change] = changed_name
+        self.new_names_list = temp_names_list.copy()
+
+        self.update_newlen()
 
 
     def set_new_to_og(self) -> None:
