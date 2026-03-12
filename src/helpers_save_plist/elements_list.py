d801089c4640e78cef33a7b89497ad9da36d64ce
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-12 17:25:00 +0100

    Numbering update

--- src/helpers_save_plist/elements_list.py
+++ src/helpers_save_plist/elements_list.py
@@ -1,8 +1,11 @@
+from src.helpers_save_plist.plist_utils import Plist_Utils
+
+
+
 class Elements_List():
     # immutable
     og_urls_list:    list[str]
     og_names_list:   list[str]
-    og_numbers_list: list[str]
     og_len: int
 
     #mutable
@@ -11,32 +14,50 @@ class Elements_List():
     new_numbers_list: list[str]
     new_index_in_og:  list[int]
     new_len: int
+    numbering: bool
+    numbering_has_zeros: bool
 
 
     def __init__(self,
                  urls_list: list[str],
                  names_list: list[str],
-                 numbers_list: list[str]
+                 numbering: bool,
+                 numbering_has_zeros: bool
     ) -> None:
-        if (len(urls_list) != len(names_list) or
-            len(urls_list) != len(numbers_list)):
+        if len(urls_list) != len(names_list):
             raise ValueError("Lists not of the same length")
 
-        self.og_urls_list     = urls_list
-        self.og_names_list    = names_list
-        self.og_numbers_list  = numbers_list
-        self.og_len = len(urls_list)
-        self.new_urls_list    = urls_list
-        self.new_names_list   = names_list
-        self.new_numbers_list = numbers_list
-        self.new_index_in_og  = [i for i in range(len(urls_list))]
-        self.new_len = len(urls_list)
+        self.og_urls_list    = urls_list
+        self.og_names_list   = names_list
+        self.og_len          = len(urls_list)
+        self.new_urls_list   = urls_list
+        self.new_names_list  = names_list
+        self.new_index_in_og = [i for i in range(len(urls_list))]
+        self.new_len         = len(urls_list)
+        self.numbering       = numbering
+        self.numbering_has_zeros = numbering_has_zeros
+
+        self.calc_numbering_list()
+
+
+    def calc_numbering_list(self) -> None:
+        if not self.numbering:
+            self.new_numbers_list = ['' for el in range(self.new_len)]
+            return
+
+        if not self.numbering_has_zeros:
+            self.new_numbers_list = [el for el in range(self.new_len)]
+            return
+
+        if self.numbering_has_zeros:
+            self.new_numbers_list = [
+                Plist_Utils.zeros_at_beginning(el, self.new_len)
+                for el in range(self.new_len)]
 
 
     def set_new_to_og(self):
         self.new_urls_list    = self.og_urls_list
         self.new_names_list   = self.og_names_list
-        self.new_numbers_list = self.og_numbers_list
         self.new_index_in_og  = [i for i in range(len(self.og_urls_list))]
         self.new_len = len(self.og_urls_list)
 
