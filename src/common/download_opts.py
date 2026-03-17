82ebc43b27fa471c9673e83a088c6d69ff100b80
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-17 21:35:51 +0100

    Setters

--- src/common/download_opts.py
+++ src/common/download_opts.py
@@ -3,9 +3,9 @@ from src.common.utils import Utils
 
 
 class Download_Opts():
-    save_format: str  #= Utils.get_val_from_settings("PLIST_SAVE_FORMAT")
-    save_path:   str  #= Utils.get_val_from_settings("SAVE_PATH")
-    ydl_opts:    dict #= Utils.get_ydl_options(save_format)
+    save_format: str
+    save_path:   str
+    ydl_opts:    dict
 
     def __init__(self) -> None:
         self.save_format = Utils.get_val_from_settings("PLIST_SAVE_FORMAT")
@@ -18,3 +18,11 @@ class Download_Opts():
 
     def reset_ydl(self) -> None:
         self.ydl_opts = Utils.get_ydl_options(self.save_format)
+
+    def set_save_format(self, new_format: str) -> None:
+        self.save_format = new_format
+        Utils.save_value_to_settings("PLIST_SAVE_FORMAT", new_format)
+
+    def set_save_path(self, new_path: str) -> None:
+        self.save_path = new_path
+        Utils.save_value_to_settings("SAVE_PATH", new_path)
