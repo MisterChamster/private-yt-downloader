31ae2636d60b565de5372bc4e75bcb04ed42f245
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-17 21:29:24 +0100

    Class update

--- src/common/download_opts.py
+++ src/common/download_opts.py
@@ -7,7 +7,14 @@ class Download_Opts():
     save_path:   str  #= Utils.get_val_from_settings("SAVE_PATH")
     ydl_opts:    dict #= Utils.get_ydl_options(save_format)
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.save_format = Utils.get_val_from_settings("PLIST_SAVE_FORMAT")
         self.save_path   = Utils.get_val_from_settings("SAVE_PATH")
-        self.ydl_opts    = Utils.get_ydl_options(self.save_format)
+        self.reset_ydl()
+
+
+    def mutate_ydl(self, ydl_key: str, ydl_val) -> None:
+        self.ydl_opts[ydl_key] = ydl_val
+
+    def reset_ydl(self) -> None:
+        self.ydl_opts = Utils.get_ydl_options(self.save_format)
