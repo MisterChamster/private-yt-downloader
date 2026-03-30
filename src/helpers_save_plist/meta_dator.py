654d27f6f1750a9e6dd16104e87808ee96bb7983
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-30 13:39:45 +0200

    Recalcing tracknums

--- src/helpers_save_plist/meta_dator.py
+++ src/helpers_save_plist/meta_dator.py
@@ -21,19 +21,25 @@ class Meta_Dator():
         self.md_date    = md_date
 
         if md_tracknumbers == [None] and not single:
-            self.md_tracknumbers = [
-                str(num+1)
-                for num
-                in range(len(md_titles))]
+            self.recalc_tracknums()
         else:
             self.md_tracknumbers = md_tracknumbers
 
 
+    def recalc_tracknums(self) -> None:
+        self.md_tracknumbers = [
+            str(num+1)
+            for num
+            in range(len(self.md_titles))]
+
+
     def pop_md_lists(self, index: int) -> None:
         self.md_titles.pop(index)
-        self.md_tracknumbers.pop(index)
+        # self.md_tracknumbers.pop(index)
+        self.recalc_tracknums()
 
 
     def pop_md_list_range(self, index_s: int, index_e: int) -> None:
         del(self.md_titles[index_s:index_e])
-        del(self.md_tracknumbers[index_s:index_e])
+        # del(self.md_tracknumbers[index_s:index_e])
+        self.recalc_tracknums()
