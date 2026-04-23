04f119983a21123fe742efc5aeb026609e1c9897
Mr Chamster <s.duda03@gmail.com>
2026-04-23 22:07:50 +0200

    Metadator __str__

--- src/helpers_save_plist/meta_dator.py
+++ src/helpers_save_plist/meta_dator.py
@@ -1,24 +1,24 @@
 class Meta_Dator():
-    md_titles:       list[str]
-    md_tracknumbers: list[str]
+    md_album:        str
     md_artist:       str
     md_date:         str
-    md_album:        str
+    md_titles:       list[str]
+    md_tracknumbers: list[str]
 
 
     def __init__(
             self,
             single:                bool,
             md_titles:        list[str],
-            md_tracknumbers:  list[str] = [None],
             md_album:        str | None = None,
             md_artist:       str | None = None,
-            md_date:         str | None = None):
+            md_date:         str | None = None,
+            md_tracknumbers:  list[str] = [None]):
 
-        self.md_titles  = md_titles
         self.md_album   = md_album
         self.md_artist  = md_artist
         self.md_date    = md_date
+        self.md_titles  = md_titles
 
         if md_tracknumbers == [None] and not single:
             self.recalc_tracknums()
@@ -43,3 +43,12 @@ class Meta_Dator():
         del(self.md_titles[index_s:index_e])
         # del(self.md_tracknumbers[index_s:index_e])
         self.recalc_tracknums()
+
+
+    def __str__(self) -> str:
+        msg = (f"Album:     {self.md_album}\n"
+               f"Artist:    {self.md_artist}\n"
+               f"Date:      {self.md_date}\n"
+               f"Titles:    {self.md_titles}\n"
+               f"Tracknums: {self.md_tracknumbers}\n")
+        return msg
